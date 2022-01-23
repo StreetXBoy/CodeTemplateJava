c7edadaab40e5ab553e9eb614250833d84de6c78
ChenXiangRay <1037880556@qq.com>
2022-01-23 23:59:03 +0800

    Initial commit 慢慢发育，加油要相信自己

---
+++ src/Dijstrka.java
@@ -0,0 +1,9 @@
+/**
+ * @author 重新做人idea基础学习
+ * @date 2022-2-23
+ */
+public class Dijstrka {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Kruskal.java
@@ -0,0 +1,6 @@
+/**
+ * @author 重新做人idea基础学习
+ * @date 2022-2-23
+ */
+public class Kruskal {
+}
