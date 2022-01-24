48f50b455ef9339dca12f1abfb6032f2227b1fbb
ChenXiangRay <1037880556@qq.com>
2022-01-24 14:09:32 +0800

    微软面试图论复习和答疑！！！！

--- src/Dijstrka.java
+++ src/Dijstrka.java
@@ -2,8 +2,57 @@
  * @author 重新做人idea基础学习
  * @date 2022-2-23
  */
+
+//不同模板有不同的写法！！！！
 public class Dijstrka {
     public static void main(String[] args) {
+        int [][] graph = init();
+        int []dis = dij(graph,0);
+        for(int data :dis){
+            System.out.println(data);
+        }
 
+    }
+    public  static  int[] dij(int [][] graph,int start){
+        int n  = graph.length;
+        int [] visited = new int[n];
+        int [] dis = new int[n];
+        for(int i=0;i<n;i++){
+            dis[i]=graph[start][i];
+        }
+        dis[start]=0;
+        visited[start]=1;
+        for(int k=1;k<n;k++){
+            int index = -1;
+            int mins = Integer.MAX_VALUE;//千万注意这个max和外面max代表不同切记不能乱来！！！
+            for(int i =0;i<n;i++){
+                if(dis[i]<mins&&visited[i]==0){
+                    index = i;
+                    mins = dis[i];
+                }
+            }
+            System.out.println("index   :"+index);
+            //dis[index]=mins;没关系的！！！
+            visited[index]=1;
+            for(int j=0;j<n;j++){
+                if(visited[j]==0){
+                    dis[j]=Math.min(dis[index]+graph[index][j],dis[j]);//引入dis数组这里必须更新dis数组，否则直接更新矩阵！！！以及大小max的坑点！！！
+                }
+            }
+        }
+        return  dis;
+    }
+
+    public  static int[][] init(){
+        int MAX =3000;
+        int graph[][] = {
+                {0,2,3,MAX,MAX,MAX},
+                {2,0,MAX,4,MAX,MAX},
+                {3,MAX,0,2,MAX,MAX},
+                {MAX,4,2,0,7,MAX,},
+                {MAX,MAX,MAX,7,0,10},
+                {MAX,MAX,MAX,MAX,10,0}
+        };
+        return graph;
     }
 }
---
+++ src/Flyord.java
@@ -0,0 +1,39 @@
+/**
+ * @author 重新做人idea基础学习
+ * @date 2022-2-24
+ */
+
+
+//相信Floyd！！！！！
+public class Flyord {
+    public static void main(String[] args) {
+
+        int [][] g =init();
+        int [][] res = flyord(g);
+        System.out.println(res[0][5]);
+    }
+
+    public  static int[][] init(){
+        int MAX =3000;
+        int graph[][] = {
+                {0,2,3,MAX,MAX,MAX},
+                {2,0,MAX,4,MAX,MAX},
+                {3,MAX,0,2,MAX,MAX},
+                {MAX,4,2,0,7,MAX,},
+                {MAX,MAX,MAX,7,0,10},
+                {MAX,MAX,MAX,MAX,10,0}
+        };
+        return graph;
+    }
+
+    public  static  int[][] flyord(int [][] graph){
+        int n =graph.length;
+        int MAX =3000;
+        for(int k=0;k<n;k++)
+            for(int i=0;i<n;i++)
+                for(int j=0;j<n;j++){
+                    graph[i][j]=Math.min(graph[i][j],graph[i][k]+graph[k][j]);//原理就是固定中间节点池化！！！
+                }
+        return  graph;
+    }
+}
